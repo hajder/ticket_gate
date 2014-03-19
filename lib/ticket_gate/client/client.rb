ea3288e2cfda07c0b7705c9bbaa646e39cedfe25
Mike Szyndel <hajder@users.noreply.github.com>
2014-03-19 11:34:24 +0100

    local url for development ticketing

--- lib/ticket_gate/client/client.rb
+++ lib/ticket_gate/client/client.rb
@@ -3,7 +3,11 @@ module TicketGate
     # ---- Setup -----------------------------------
     # ----------------------------------------------
     version 1
-    base_uri 'https://api.stagelink.com'
+    if Rails.env.development?
+      base_uri 'http://ticketing.dev'
+    else
+      base_uri 'https://api.stagelink.com'
+    end
 
     def initialize access_token = nil
       raise 'Please initialize with your api access token' unless access_token
